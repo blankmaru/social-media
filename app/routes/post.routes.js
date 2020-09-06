7be616082e0c522e3f2dea314322526e17235f8e
blankmaru <coopskywavy@gmail.com>
2020-09-06 19:32:14 +0300

    Release posts functionality from server

--- app/routes/post.routes.js
+++ app/routes/post.routes.js
@@ -8,4 +8,31 @@ router.route('/').get((req, res) => {
         .catch(err => console.error(err));
 });
 
+router.route('/add').post((req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    const author = req.body.author;
+    const likes = req.body.likes;
+    const comments = req.body.comments;
+
+    const newPost = new Post({
+        title,
+        content,
+        author,
+        likes,
+        comments
+    });
+
+    newPost
+        .save()
+        .then(() => res.json('Post added'))
+        .catch(err => console.error(err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Post.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Post deleted'))
+        .catch(err => res.status(400).json({ error: err }));
+});
+
 module.exports = router;
